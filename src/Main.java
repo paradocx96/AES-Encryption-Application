9cb33e4750f8b8bf2e4d090df83b001cd0181581
Navinda Chandrasiri <it19180526@my.sliit.lk>
2022-08-17 01:13:24 +0530

    Project Networked Program Updated

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         try {
+            System.out.println("NETWORKED PROGRAM");
+            System.out.println("\n@Sender");
+
+            // Generate a secret key
             AES_ENCRYPTION aes_encryption = new AES_ENCRYPTION();
             aes_encryption.init();
-            String encryptedData = aes_encryption.encrypt("Hello, welcome to the encryption world");
+
+            // Getting user message
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter Message: ");
+            String sender_message = scanner.nextLine();
+            System.out.println("Sender Message: " + sender_message);
+
+            // Encrypt user message
+            String encryptedData = aes_encryption.encrypt(sender_message);
+
+            // Display the encrypted string
+            System.out.println("Encrypted Data: " + encryptedData);
+            System.out.println("\n@Receiver");
+            System.out.println("Encrypted Data: " + encryptedData);
+
+            // Decrypt the user message
             String decryptedData = aes_encryption.decrypt(encryptedData);
 
-            System.out.println("Encrypted Data : " + encryptedData);
-            System.out.println("Decrypted Data : " + decryptedData);
+            // Display original message
+            System.out.println("Received Message: " + decryptedData);
         } catch (Exception ignored) {
         }
     }
